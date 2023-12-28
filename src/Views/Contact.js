68f55c757c2252146a3e24b8e996723f01877443
Ahmed Wael <ahmedwael978@gmail.com>
2023-12-28 23:50:54 +0200

    Added ContactUs Component + Cart Item Component

--- src/Views/Contact.js
+++ src/Views/Contact.js
@@ -1,11 +1,15 @@
 import React from "react";
 import NavBar from "../Components/NavBar/Menu";
-
+import {ContactUs} from '../Components/Contact Us/ContactUs'
 const Contact =() =>{
     return(
         <div>
             <NavBar/>
-
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <ContactUs/>
             {/*<h1>This is contact page</h1>*/}
         </div>
     );
