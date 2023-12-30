dc347c4ffeb8ad10693d3c48cafb2fa8cd5dc59d
Brwana <brwana110@gmail.com>
2023-12-30 02:03:13 +0200

    user_info

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import About from "./Views/About"
 import Mobiles from "./Views/Mobiles"
 import Laptops from "./Views/Laptops"
 import Cart from "./Views/Cart"
+import User_info from "./Views/User_info";
 import React from "react";
 
 
@@ -25,6 +26,7 @@ function App() {
                     <Route path="/mobile" element={<Mobiles/>}></Route>
                     <Route path="/laptop" element={<Laptops/>}></Route>
                     <Route path="/cart" element={<Cart/>}></Route>
+                    <Route path="/user_info" element={<User_info/>}></Route>
                 </Routes>
             </BrowserRouter>
 
