f3230482f59d4647aef11e908377282b07d01d62
Brwana <brwana110@gmail.com>
2023-12-31 01:04:32 +0200

    updated_navbar

--- src/Components/NavBar/Menu.jsx
+++ src/Components/NavBar/Menu.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import './Menu.css'
 import logo from '../Assets/logo.gif'
+import user_icon from '../Assets/user_nav.png'
+import { memo, useEffect, useState } from "react";
+import User_info from "../../Views/User_info";
+
+
 const Menu = () => {
+
     return (
         <nav>
             <div className="logo-container">
                 <img className='Logo' src={logo} alt='mobilaty logo'/>
                 <a className='Brand' href={'/home'}>Mobilaty</a>
+            </div>
+            <div className="logo2-container">
+                <img src={user_icon}/>
+                <ul className="subnav">
+                    <li ><button className="btui">Logout</button></li><hr/>
+                    <li className="ui2"><a href={'/user_info'}>User Info</a></li>
+                </ul>
+
             </div>
 
             <div className="links">
